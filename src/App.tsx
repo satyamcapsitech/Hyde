ef19e46bc7e684f95908d751f02553ffe914cc16
satyam <satyam.singhcapsitech.com>
2024-09-12 13:31:33 +0530

    code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import Admin from "./components/Admin/Admin.tsx";
 import User from "./components/User/User.tsx";
 
 function App() {
-  
   return (
     <Router>
       <div className="main-container">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
